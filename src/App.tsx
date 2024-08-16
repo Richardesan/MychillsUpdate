e1e76fb535c5afba74f97d2b233ff326107b7e62
tito <olayiwola.titofunmi212@gmail.com>
2024-08-17 00:09:20 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { CssBaseline, Button } from "@mui/joy";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <div style={{ padding: "20px" }}>
+        <Button variant="solid">Hello, Joy UI!</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
